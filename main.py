0ea719214dcce671915a7d39ea56d9985879b84c
Vadim Colcev <vadimkolchev@gmail.com>
2021-07-01 21:57:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pathlib
+
+
+def retrieve_content(url: str) -> BeautifulSoup:
+    """returns soup of webpage for passed url"""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
+
+
+# create text file TODO
+path = pathlib.Path("/Users/vdmclcv/bs4tests")
+if path.exists():
+    file = path / "python_doc.txt"
+    pathlib.Path.touch(file)
+
+# get links from index page
+index_page_soup = retrieve_content("https://docs.python.org/3/tutorial/")
+links_bs4 = index_page_soup.select("li > a.reference.internal")
+links_bs4_filtered = filter(lambda link: '#' not in link.get('href'), links_bs4)
+
+# get content from each of links
+for entry in links_bs4_filtered:
+    # create URL
+    entry_url = "https://docs.python.org/3/tutorial/" + entry.get('href')
+    # fetch URL content
+    url_soup = retrieve_content(entry_url)
+    # fetch section text in URL content
+    section_text = url_soup.select("div.section")[0].get_text()
+    # open file for writing
+    with open(file, 'a') as target_file:
+        target_file.write(section_text)
+        target_file.write('\n\n\n')
